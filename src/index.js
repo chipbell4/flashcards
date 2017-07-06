c3939be6fdb9acff14248e5f843b9657016fbbac
Chip Bell <cbell@pbs.org>
2017-07-06 13:49:12 -0400

    Gluing up to the UI

--- src/index.js
+++ src/index.js
@@ -1,34 +1,11 @@
-window.App = {
-  init: function() {
-    this.sentenceContainer = document.getElementById('sentence');
-    this.refreshButton = document.getElementById('refresh');
-    this.refreshButton.addEventListener('click', this.refresh.bind(this));
-  },
+var generate = require('./generate.js');
 
-  isIgnorableSentence: function() {
-    return false;
-  },
+var sentenceContainer = document.getElementById('sentence');
 
-  sentencesRemaining: [],
-
-  getSentence: function() {
-    if(this.sentencesRemaining.length > 0) {
-      return Promise.resolve(this.sentencesRemaining.pop());
-    }
-
-    return fetch('https://randomwordgenerator.com/json/sentences.json')
-      .then(response => response.json())
-      .then(rawJson => rawJson.data.map(sentenceObj => sentenceObj.sentence))
-      .then(sentences => {
-        this.sentencesRemaining = sentences.filter(sentence => !this.isIgnorableSentence(sentence));
-        return this.sentencesRemaining.pop();
-      });
-  },
-
-  refresh: function() {
-    this.getSentence().then(sentence => this.sentenceContainer.innerText = sentence);
-  }
+var refresh = function() {
+  sentenceContainer.innerText = generate();
 };
 
-window.App.init();
-window.App.refresh();
+var refreshButton = document.getElementById('refresh');
+document.addEventListener('click', refresh);
+refresh();
