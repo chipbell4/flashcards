0abb883de13d04f4188370cb148391cd29077580
Chip Bell <cbell@pbs.org>
2017-08-06 09:58:58 -0400

    Basic toggling

--- src/index.js
+++ src/index.js
@@ -12,4 +12,23 @@ document.addEventListener('keydown', function(evt) {
     refresh();
   }
 })
+
+document.getElementById('tab').addEventListener('click', function(evt) {
+  // don't regenerate a sentence, since that wasn't user intention
+  evt.stopPropagation();
+
+  var tab = evt.target;
+
+  var configContainer = document.getElementById('config-container');
+  configContainer.classList.toggle('showing');
+
+  if(configContainer.classList.contains('showing')) {
+    tab.innerHTML = '▼';
+  } else {
+    tab.innerHTML = '▲';
+  }
+
+  return false;
+});
+
 refresh();
